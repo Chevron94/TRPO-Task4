8133a7eb63d44de6eed5c0a991f774318c35c6b6
Chevron94 <chevron19941@gmail.com>
2014-10-03 00:00:36 +0400

    First

---
+++ Task4/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Task4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.InputText = new System.Windows.Forms.TextBox();
+            this.OutPut = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(98, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InputText
+            // 
+            this.InputText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputText.Location = new System.Drawing.Point(12, 34);
+            this.InputText.Name = "InputText";
+            this.InputText.Size = new System.Drawing.Size(260, 20);
+            this.InputText.TabIndex = 1;
+            this.InputText.TextChanged += new System.EventHandler(this.InputText_TextChanged);
+            // 
+            // OutPut
+            // 
+            this.OutPut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.OutPut.FormattingEnabled = true;
+            this.OutPut.Location = new System.Drawing.Point(12, 60);
+            this.OutPut.Name = "OutPut";
+            this.OutPut.Size = new System.Drawing.Size(260, 147);
+            this.OutPut.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Входная последовательность:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutPut);
+            this.Controls.Add(this.InputText);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Подпоследовательность";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox InputText;
+        private System.Windows.Forms.ListBox OutPut;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Task4/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Task4
+{
+    enum States { Begin, Sum }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OutPut.Items.Clear();
+            string[] input = InputText.Text.Split(' ');
+            int MaxSum = 0;
+            int CurSum = 0;
+            int MinLength = Int32.MaxValue;
+            int CurLength = 0;
+            int MinStart = 0;
+            int MinEnd = 0;
+            int i = 0;
+            int j = 0;
+            int Len = input.Length;
+            States State = States.Begin;
+            do
+            {
+                int tmp;
+                if (Int32.TryParse(input[i].Trim(), out tmp))
+                {
+                    switch (State)
+                    {
+                        case States.Begin:
+                            CurSum = 0;
+                            CurLength = 0;
+                            break;
+                        case States.Sum:
+                            break;
+                    }
+                    CurSum += tmp;
+                    CurLength++;
+                    if (CurSum <= 0)
+                    {
+                        State = States.Begin;
+                    }
+                    else
+                    {
+                        State = States.Sum;
+                        if (CurSum >= MaxSum)
+                        {
+                            if (CurSum > MaxSum)
+                            {
+                                MaxSum = CurSum;
+                                MinLength = CurLength;
+                                MinStart = j - CurLength + 1;
+                                MinEnd = j;
+                            }
+                            else if (CurLength < MinLength)
+                            {
+                                MinLength = CurLength;
+                                MinStart = j - CurLength + 1;
+                                MinEnd = j;
+                            }
+                        }
+                    }
+                    j++;
+                }
+                i++;
+            } while (i < Len);
+            if (MaxSum == 0)
+                OutPut.Items.Add("Отсутствуют элементы > 0");
+            else
+            {
+                OutPut.Items.Add("Максимальная сумма = " + MaxSum.ToString());
+                OutPut.Items.Add("Длина последовательности : " + MinLength.ToString());
+                OutPut.Items.Add("Начало : " + MinStart.ToString());
+                OutPut.Items.Add("Конец : " + MinEnd.ToString()); 
+            }
+        }
+
+        private void InputText_TextChanged(object sender, EventArgs e)
+        {
+            OutPut.Items.Clear();
+        }
+    }
+}
